1c8f156558e877ea03e51a1762341e2b49808176
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2022-09-13 05:21:27 -0700

    checked Register

--- app/src/main/java/com/example/coffeeshop/presentation/ui/fragments/PreferencesFragment.kt
+++ app/src/main/java/com/example/coffeeshop/presentation/ui/fragments/PreferencesFragment.kt
@@ -2,15 +2,13 @@ package com.example.coffeeshop.presentation.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-
 import android.view.View
-
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-
 import androidx.navigation.fragment.findNavController
 import com.example.coffeeshop.R
+
 
 class PreferencesFragment : Fragment(R.layout.fragment_preferences) {
     private lateinit var totalPrice: TextView
@@ -21,19 +19,22 @@ class PreferencesFragment : Fragment(R.layout.fragment_preferences) {
     private lateinit var price: TextView
     private lateinit var prefOne: ImageView
     private lateinit var prefTwo: ImageView
-    private lateinit var oneSugar: ImageView
-    private lateinit var  add: ImageView
+    private lateinit var oneSuger: ImageView
+    private lateinit var add: ImageView
     private lateinit var remove: ImageView
+    private lateinit var image: ImageView
+    private lateinit var name: TextView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val prefBtn = view.findViewById<Button>(R.id.btn_add_to_cart)
         smallCoffe = view.findViewById(R.id.iv_coffee_small)
         midCoffe = view.findViewById(R.id.iv_coffee_med)
         largeCoffe = view.findViewById(R.id.iv_coffe_large)
-        val onSugar = view.findViewById<ImageView>(R.id.iv_no_sugar)
-        oneSugar = view.findViewById(R.id.iv_sugar1)
-        val twoSugar = view.findViewById<ImageView>(R.id.iv_sugar2)
-        val threeSugar = view.findViewById<ImageView>(R.id.iv_sugar3)
+        val noSuger = view.findViewById<ImageView>(R.id.iv_no_sugar)
+        oneSuger = view.findViewById(R.id.iv_sugar1)
+        val twoSuger = view.findViewById<ImageView>(R.id.iv_sugar2)
+        val threeSuger = view.findViewById<ImageView>(R.id.iv_sugar3)
         prefOne = view.findViewById(R.id.iv_pref1)
         prefTwo = view.findViewById(R.id.iv_pref2)
         count = view.findViewById(R.id.counter)
@@ -41,7 +42,8 @@ class PreferencesFragment : Fragment(R.layout.fragment_preferences) {
         price = view.findViewById(R.id.price)
         add = view.findViewById(R.id.add)
         remove = view.findViewById(R.id.remove)
-
+        image = view.findViewById(R.id.image)
+        name = view.findViewById(R.id.category)
         prefBtn.setOnClickListener {
             findNavController().navigate(PreferencesFragmentDirections.actionPreferencesFragmentToCartFragment())
         }
@@ -72,46 +74,46 @@ class PreferencesFragment : Fragment(R.layout.fragment_preferences) {
             smallCoffe.tag = R.color.gray
             calculateTotal()
         }
-        onSugar.setOnClickListener {
-            onSugar.setColorFilter(R.color.black)
-            oneSugar.clearColorFilter()
-            twoSugar.clearColorFilter()
-            threeSugar.clearColorFilter()
-            onSugar.tag = R.color.black
-            oneSugar.tag = R.color.gray
-            twoSugar.tag = R.color.gray
-            threeSugar.tag = R.color.black
+        noSuger.setOnClickListener {
+            noSuger.setColorFilter(R.color.black)
+            oneSuger.clearColorFilter()
+            twoSuger.clearColorFilter()
+            threeSuger.clearColorFilter()
+            noSuger.tag = R.color.black
+            oneSuger.tag = R.color.gray
+            twoSuger.tag = R.color.gray
+            threeSuger.tag = R.color.black
         }
-        oneSugar.setOnClickListener {
-            oneSugar.setColorFilter(R.color.black)
-            onSugar.clearColorFilter()
-            twoSugar.clearColorFilter()
-            threeSugar.clearColorFilter()
-            oneSugar.tag = R.color.black
-            onSugar.tag = R.color.gray
-            twoSugar.tag = R.color.gray
-            threeSugar.tag = R.color.gray
+        oneSuger.setOnClickListener {
+            oneSuger.setColorFilter(R.color.black)
+            noSuger.clearColorFilter()
+            twoSuger.clearColorFilter()
+            threeSuger.clearColorFilter()
+            oneSuger.tag = R.color.black
+            noSuger.tag = R.color.gray
+            twoSuger.tag = R.color.gray
+            threeSuger.tag = R.color.gray
             //calculateTotal()
         }
-        twoSugar.setOnClickListener {
-            twoSugar.setColorFilter(R.color.black)
-            oneSugar.clearColorFilter()
-            onSugar.clearColorFilter()
-            threeSugar.clearColorFilter()
-            oneSugar.tag = R.color.gray
-            onSugar.tag = R.color.gray
-            twoSugar.tag = R.color.black
-            threeSugar.tag = R.color.gray
+        twoSuger.setOnClickListener {
+            twoSuger.setColorFilter(R.color.black)
+            oneSuger.clearColorFilter()
+            noSuger.clearColorFilter()
+            threeSuger.clearColorFilter()
+            oneSuger.tag = R.color.gray
+            noSuger.tag = R.color.gray
+            twoSuger.tag = R.color.black
+            threeSuger.tag = R.color.gray
         }
-        threeSugar.setOnClickListener {
-            threeSugar.setColorFilter(R.color.black)
-            oneSugar.clearColorFilter()
-            twoSugar.clearColorFilter()
-            onSugar.clearColorFilter()
-            oneSugar.tag = R.color.gray
-            onSugar.tag = R.color.gray
-            twoSugar.tag = R.color.gray
-            threeSugar.tag = R.color.black
+        threeSuger.setOnClickListener {
+            threeSuger.setColorFilter(R.color.black)
+            oneSuger.clearColorFilter()
+            twoSuger.clearColorFilter()
+            noSuger.clearColorFilter()
+            oneSuger.tag = R.color.gray
+            noSuger.tag = R.color.gray
+            twoSuger.tag = R.color.gray
+            threeSuger.tag = R.color.black
         }
         prefOne.setOnClickListener {
             prefOne.setColorFilter(R.color.black)
@@ -128,13 +130,13 @@ class PreferencesFragment : Fragment(R.layout.fragment_preferences) {
 
         }
         add.setOnClickListener {
-            count.text=(count.text.toString().toInt()+1).toString()
+            count.text = (count.text.toString().toInt() + 1).toString()
             calculateTotal()
         }
 
         remove.setOnClickListener {
-            if(count.text.toString().toInt()>0){
-                count.text=(count.text.toString().toInt()-1).toString()
+            if (count.text.toString().toInt() > 0) {
+                count.text = (count.text.toString().toInt() - 1).toString()
             }
             calculateTotal()
         }
@@ -142,24 +144,25 @@ class PreferencesFragment : Fragment(R.layout.fragment_preferences) {
         calculateTotal()
 
     }
-    private fun defaultSelect(){
+
+    private fun defaultSelect() {
         midCoffe.setColorFilter(R.color.black)
         midCoffe.tag = R.color.black
-        oneSugar.setColorFilter(R.color.black)
-        oneSugar.tag = R.color.black
+        oneSuger.setColorFilter(R.color.black)
+        oneSuger.tag = R.color.black
     }
 
-    private fun calculateTotal() {
+    fun calculateTotal() {
         val num = count.text.toString().toInt()
         var total = 0.0
         val smPrice = price.text.toString().toDouble()
         if (midCoffe.tag.equals(R.color.black)) {
-            total = smPrice * num*1.5
+            total = smPrice * num * 1.5
         } else if (smallCoffe.tag.equals(R.color.black)) {
             total = smPrice * num
         } else if (largeCoffe.tag.equals(R.color.black)) {
             total = smPrice * 2 * num
         }
-        totalPrice.text = ("$total EGP")
+        totalPrice.text = ("${total.toString()} EGP")
     }
 }
--- app/src/main/java/com/example/coffeeshop/presentation/ui/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/coffeeshop/presentation/ui/fragments/RegisterFragment.kt
@@ -2,9 +2,12 @@ package com.example.coffeeshop.presentation.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.coffeeshop.R
 import com.example.coffeeshop.data.model.RegisterRequest
 import com.example.coffeeshop.util.NetworkResult
@@ -12,30 +15,53 @@ import com.example.coffeeshop.presentation.viewmodel.RegisterViewModel
 
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
-    private val registerViewModel:RegisterViewModel by viewModels()
+    private val registerViewModel: RegisterViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        registerViewModel.register(RegisterRequest("Seif","seeif2@gmail.com","123456abcdew"))
-        /*val loginTx=view.findViewById<TextView>(R.id.tx_goLogin)
-        loginTx.setOnClickListener{
-            findNavController().navigate(
+        val etFullName = view.findViewById<EditText>(R.id.tx_fullName)
+        val etEmail = view.findViewById<EditText>(R.id.tx_email)
+        val etPass = view.findViewById<EditText>(R.id.tx_pass)
+        val etcPass = view.findViewById<EditText>(R.id.tx_conPass)
+        val btn = view.findViewById<Button>(R.id.btn_register)
+        btn.setOnClickListener {
+            val email = etEmail?.text.toString()
+            val password = etPass?.text.toString()
+            val username = etFullName?.text.toString()
+            val repeatedPass = etcPass?.text.toString()
+            if (email.isEmpty() || password.isEmpty() || username.isEmpty() || repeatedPass.isEmpty()) {
+                Toast.makeText(
+                    requireContext(),
+                    "please fill all required fields",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (password != repeatedPass) {
+                Toast.makeText(
+                    requireContext(),
+                    "password field is not equal to repeated password field",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                registerViewModel.register(RegisterRequest(username, email, password))
+                registerViewModel.liveData.observe(viewLifecycleOwner) {
+                    it?.let { response ->
+                        when (response) {
+                            //
+                            is NetworkResult.Success -> findNavController().navigate(
+                                RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
+                            )
+                            is NetworkResult.Error -> Toast.makeText(
+                                requireContext(),
+                                "user: ${response.message}",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            else -> {
 
-                RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
-            )
-        }*/
-        registerViewModel.liveData.observe(viewLifecycleOwner){
-            it?.let { response ->
-                when(response){
-                    //
-                    is NetworkResult.Success -> Toast.makeText(requireContext(), "user: ${response.data.toString()}", Toast.LENGTH_LONG).show()
-                    is NetworkResult.Error -> Toast.makeText(requireContext(), "user: ${response.message}", Toast.LENGTH_LONG).show()
-                    else ->{
-
+                            }
+                        }
                     }
                 }
             }
         }
-
     }
 
 }
